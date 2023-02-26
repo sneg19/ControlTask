f909570b78d14dfccc319e4d2a5b59898b095aa0
sneg <sneg.sbg@gmail.com>
2023-02-26 19:16:12 +0300

    GetArray

--- Program.cs
+++ Program.cs
@@ -1,19 +1,14 @@
 ﻿// // Написать программа, которая создает массив из строк данного массива. которые по длинне меньше 3-х символов
 
-
-string[] strArray = new string[4];
-strArray[0] = "one";
-strArray[1] = "twotwo";
-strArray[2] = "thr";
-strArray[3] = "four";
-
-string[] strMatrix = new string[4];
-for(int i=0; i<4; i++)
+string[] GetArray(int size)
 {
-    if(strArray[i].Length < 4) 
+    string[] strArray = new string[size];
+    for(int i = 0; i < strArray.Length; i++)
     {
-        strMatrix[i] = strArray[i];
-    } 
+        System.Console.Write($"Введите стрококу, которая запишется в {i}-й элемент массива: ");
+        strArray[i] = Console.ReadLine()!;
+    }
+    return strArray[];
 }
-System.Console.WriteLine(string.Join(",", strMatrix));
-System.Console.WriteLine(string.Join(",", strArray));
+
+ 

b3b552a7d7f135ea83a0dd533aaf2c293c8d82d1
sneg <sneg.sbg@gmail.com>
2023-02-26 17:38:59 +0300

    Черновик

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// // Написать программа, которая создает массив из строк данного массива. которые по длинне меньше 3-х символов
+
+
+string[] strArray = new string[4];
+strArray[0] = "one";
+strArray[1] = "twotwo";
+strArray[2] = "thr";
+strArray[3] = "four";
+
+string[] strMatrix = new string[4];
+for(int i=0; i<4; i++)
+{
+    if(strArray[i].Length < 4) 
+    {
+        strMatrix[i] = strArray[i];
+    } 
+}
+System.Console.WriteLine(string.Join(",", strMatrix));
+System.Console.WriteLine(string.Join(",", strArray));
